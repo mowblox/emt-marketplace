614695f2b04d30e9f8cbe67386659858522d0bc4
Michael Hammond <33749130+mickeymond@users.noreply.github.com>
2023-10-30 20:44:36 +0000

    Setup expt token (#2)
    * wip: using openzeppelin wizard to generate expert token contract
    
    * wip: one deploy script for all

---
+++ blockchain/contracts/ExpertToken.sol
@@ -0,0 +1,53 @@
+// SPDX-License-Identifier: APACHE
+pragma solidity ^0.8.20;
+
+import "@openzeppelin/contracts/token/ERC1155/ERC1155.sol";
+import "@openzeppelin/contracts/access/AccessControl.sol";
+
+/// @custom:security-contact odafe@mowblox.com
+contract ExpertToken is ERC1155, AccessControl {
+    bytes32 public constant URI_SETTER_ROLE = keccak256("URI_SETTER_ROLE");
+    bytes32 public constant MINTER_ROLE = keccak256("MINTER_ROLE");
+
+    constructor(
+        address defaultAdmin,
+        address minter
+    )
+        ERC1155(
+            "https://firebasestorage.googleapis.com/v0/b/emt-marketplace/{id}.json"
+        )
+    {
+        _grantRole(DEFAULT_ADMIN_ROLE, defaultAdmin);
+        _grantRole(MINTER_ROLE, minter);
+    }
+
+    function setURI(string memory newuri) public onlyRole(URI_SETTER_ROLE) {
+        _setURI(newuri);
+    }
+
+    function mint(
+        address account,
+        uint256 id,
+        uint256 amount,
+        bytes memory data
+    ) public onlyRole(MINTER_ROLE) {
+        _mint(account, id, amount, data);
+    }
+
+    function mintBatch(
+        address to,
+        uint256[] memory ids,
+        uint256[] memory amounts,
+        bytes memory data
+    ) public onlyRole(MINTER_ROLE) {
+        _mintBatch(to, ids, amounts, data);
+    }
+
+    // The following functions are overrides required by Solidity.
+
+    function supportsInterface(
+        bytes4 interfaceId
+    ) public view override(ERC1155, AccessControl) returns (bool) {
+        return super.supportsInterface(interfaceId);
+    }
+}
