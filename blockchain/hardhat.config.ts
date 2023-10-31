4e321f589b39ddd271297338db0d33c60d519a0c
Michael Hammond <33749130+mickeymond@users.noreply.github.com>
2023-10-31 13:40:16 +0000

    Continue expt token setup (#4)
    * wip: adding ExpertToken.ts file for testing
    
    * wip: updating the test file to match expt
    
    * wip: adding a default fake private key
    
    * wip: renaming environment variables

--- blockchain/hardhat.config.ts
+++ blockchain/hardhat.config.ts
@@ -1,4 +1,5 @@
 import { HardhatUserConfig } from "hardhat/config";
+import crypto from "crypto";
 import "@nomicfoundation/hardhat-toolbox";
 import dotenv from "dotenv";
 
@@ -9,11 +10,11 @@ const config: HardhatUserConfig = {
   networks: {
     topos: {
       url: 'https://rpc.topos-subnet.testnet-1.topos.technology',
-      accounts: [process.env.PRIVATE_KEY as string]
+      accounts: [process.env.PRIVATE_KEY as string || crypto.randomBytes(32).toString('hex')]
     },
     incal: {
       url: 'https://rpc.incal.testnet-1.topos.technology',
-      accounts: [process.env.PRIVATE_KEY as string]
+      accounts: [process.env.PRIVATE_KEY as string || crypto.randomBytes(32).toString('hex')]
     }
   },
   gasReporter: {
