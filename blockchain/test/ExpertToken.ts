2abadd25615afef2931f6985cb697f08b97ede93
mickeymond <33749130+mickeymond@users.noreply.github.com>
2023-10-31 00:17:56 +0000

    wip: updating the test file to match expt

--- blockchain/test/ExpertToken.ts
+++ blockchain/test/ExpertToken.ts
@@ -3,29 +3,24 @@ import { anyValue } from "@nomicfoundation/hardhat-chai-matchers/withArgs";
 import { expect } from "chai";
 import { ethers } from "hardhat";
 
-describe("MentorToken", function () {
+describe("ExpertToken", function () {
     // We define a fixture to reuse the same setup in every test.
     // We use loadFixture to run this setup once, snapshot that state,
     // and reset Hardhat Network to that snapshot in every test.
-    async function deployMentorTokenFixture() {
+    async function deployExpertTokenFixture() {
         // Contracts are deployed using the first signer/account by default
         const [owner, minter] = await ethers.getSigners();
 
-        const MentorToken = await ethers.getContractFactory("MentorToken");
-        const mentorToken = await MentorToken.deploy(owner.address, minter.address);
+        const ExpertToken = await ethers.getContractFactory("ExpertToken");
+        const expertToken = await ExpertToken.deploy(owner.address, minter.address);
 
-        return { mentorToken, owner, minter };
+        return { expertToken, owner, minter };
     }
 
     // Test Goes Below
     describe("Deployment", function () {
-        it("deploys mentor token with all expected defaults", async function () {
-            const { mentorToken } = await loadFixture(deployMentorTokenFixture);
-
-            console.log("Total Supply: ", await mentorToken.totalSupply());
-            console.log("Name: ", await mentorToken.name());
-            console.log("Symbol: ", await mentorToken.symbol());
-            console.log("Decimals: ", await mentorToken.decimals());
+        it("deploys expert token with all expected defaults", async function () {
+            const { expertToken } = await loadFixture(deployExpertTokenFixture);
         })
     });
 });
