d4e6b102020e1608ed7cb70e4b8ee5a88c5b5cd9
Jovells <Jovells.appiah@gmail.com>
2023-11-01 04:07:30 +0000

    Added Deployment expect

--- blockchain/test/MentorToken.ts
+++ blockchain/test/MentorToken.ts
@@ -22,36 +22,37 @@ describe("MentorToken", function () {
     it("deploys mentor token with all expected defaults", async function () {
       const { mentorToken } = await loadFixture(deployMentorTokenFixture);
 
-      console.log("Total Supply: ", await mentorToken.totalSupply());
-      console.log("Name: ", await mentorToken.name());
-      console.log("Symbol: ", await mentorToken.symbol());
-      console.log("Decimals: ", await mentorToken.decimals());
+      const TotalSupply = await mentorToken.totalSupply();
+      const Name =  await mentorToken.name();
+      const Symbol =  await mentorToken.symbol();
+      const Decimals =  await mentorToken.decimals();
+      expect([TotalSupply, Name, Symbol, Decimals]).to.deep.equal([0n, "Mentor Token", "MENT", 0n]);
     })
   });
   describe("Minting", function () {
     it("Should mint correctly", async function () {
       const { mentorToken, minter, user } = await loadFixture(deployMentorTokenFixture);
 
-      const amountToMint = 1n
+      const amountToMint = 1n;
       const initialBalance = await mentorToken.balanceOf(user.address);
       const initialTotalSupply = await mentorToken.totalSupply();
+      
       await mentorToken.connect(minter).mint(user.address, amountToMint);
+      
       const finalBalance = await mentorToken.balanceOf(user.address);
       const finalTotalSupply = await mentorToken.totalSupply();
+      
       expect(finalBalance).to.equal(initialBalance + amountToMint);
       expect(finalTotalSupply).to.equal(initialTotalSupply + amountToMint);
     })
+
     it("Should not mint if not minter", async function () {
       const { mentorToken, user } = await loadFixture(deployMentorTokenFixture);
-
-      const amountToMint = 1n
+      const amountToMint = 1n;
       const initialBalance = await mentorToken.balanceOf(user.address);
       const initialTotalSupply = await mentorToken.totalSupply();
+      
       await expect(mentorToken.connect(user).mint(user.address, 1)).to.be.revertedWithCustomError(mentorToken, 'AccessControlUnauthorizedAccount')
-      const finalBalance = await mentorToken.balanceOf(user.address);
-      const finalTotalSupply = await mentorToken.totalSupply();
-      expect(finalBalance).to.equal(initialBalance);
-      expect(finalTotalSupply).to.equal(initialTotalSupply);
     })
   });
 });
