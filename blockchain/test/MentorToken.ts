1c6aed4982feba4cc189e1055fd5555ecfbd45fc
Jovells <35954298+Jovells@users.noreply.github.com>
2023-11-01 11:21:02 +0000

    Finished testing mentor token (#9)
    * Finished Testing Mentor Token
    
    * Added Deployment expect

--- blockchain/test/MentorToken.ts
+++ blockchain/test/MentorToken.ts
@@ -9,12 +9,12 @@ describe("MentorToken", function () {
   // and reset Hardhat Network to that snapshot in every test.
   async function deployMentorTokenFixture() {
     // Contracts are deployed using the first signer/account by default
-    const [owner, minter] = await ethers.getSigners();
+    const [owner, minter, user] = await ethers.getSigners();
 
     const MentorToken = await ethers.getContractFactory("MentorToken");
     const mentorToken = await MentorToken.deploy(owner.address, minter.address);
 
-    return { mentorToken, owner, minter };
+    return { mentorToken, owner, minter, user };
   }
 
   // Test Goes Below
@@ -22,10 +22,37 @@ describe("MentorToken", function () {
     it("deploys mentor token with all expected defaults", async function () {
       const { mentorToken } = await loadFixture(deployMentorTokenFixture);
 
-      console.log("Total Supply: ", await mentorToken.totalSupply());
-      console.log("Name: ", await mentorToken.name());
-      console.log("Symbol: ", await mentorToken.symbol());
-      console.log("Decimals: ", await mentorToken.decimals());
+      const TotalSupply = await mentorToken.totalSupply();
+      const Name =  await mentorToken.name();
+      const Symbol =  await mentorToken.symbol();
+      const Decimals =  await mentorToken.decimals();
+      expect([TotalSupply, Name, Symbol, Decimals]).to.deep.equal([0n, "Mentor Token", "MENT", 0n]);
+    })
+  });
+  describe("Minting", function () {
+    it("Should mint correctly", async function () {
+      const { mentorToken, minter, user } = await loadFixture(deployMentorTokenFixture);
+
+      const amountToMint = 1n;
+      const initialBalance = await mentorToken.balanceOf(user.address);
+      const initialTotalSupply = await mentorToken.totalSupply();
+      
+      await mentorToken.connect(minter).mint(user.address, amountToMint);
+      
+      const finalBalance = await mentorToken.balanceOf(user.address);
+      const finalTotalSupply = await mentorToken.totalSupply();
+      
+      expect(finalBalance).to.equal(initialBalance + amountToMint);
+      expect(finalTotalSupply).to.equal(initialTotalSupply + amountToMint);
+    })
+
+    it("Should not mint if not minter", async function () {
+      const { mentorToken, user } = await loadFixture(deployMentorTokenFixture);
+      const amountToMint = 1n;
+      const initialBalance = await mentorToken.balanceOf(user.address);
+      const initialTotalSupply = await mentorToken.totalSupply();
+      
+      await expect(mentorToken.connect(user).mint(user.address, 1)).to.be.revertedWithCustomError(mentorToken, 'AccessControlUnauthorizedAccount')
     })
   });
 });
