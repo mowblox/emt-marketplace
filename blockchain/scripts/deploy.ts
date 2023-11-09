7890260b95d61bee4c90c155f1636fcdbbd3a115
mickeymond <33749130+mickeymond@users.noreply.github.com>
2023-11-09 04:56:46 +0000

    setting mentor token in deployment script

--- blockchain/scripts/deploy.ts
+++ blockchain/scripts/deploy.ts
@@ -12,25 +12,31 @@ async function main() {
   // Deploy Marketplace Contract
   const emtMarketplace = await ethers.deployContract("EMTMarketplace", [defaultAdmin]);
   emtMarketplace.waitForDeployment();
-  // emtMarketplace.deploymentTransaction()?.chainId
   console.log("EMT Marketplace deployed at: ", emtMarketplace.target);
 
   const minter = process.env.TOKEN_MINTER || emtMarketplace.target;
 
+  // Deploy Mentor Token
   const mentorToken = await ethers.deployContract("MentorToken", [
     defaultAdmin,
     minter
   ]);
   await mentorToken.waitForDeployment();
   console.log("Mentor Token deployed at: ", mentorToken.target);
-  
+
+  // Deploy Expert Token
   const expertToken = await ethers.deployContract("ExpertToken", [
     defaultAdmin,
     minter
   ]);
   await expertToken.waitForDeployment();
   console.log("Expert Token deployed at: ", expertToken.target);
-  
+
+  // Set Ment Token in Marketplace Contract
+  console.log("Mentor Token Before: ", await emtMarketplace.mentTokenAddress());
+  await emtMarketplace.setMentToken(mentorToken.target);
+  console.log("Mentor Token After: ", await emtMarketplace.mentTokenAddress());
+
   const chainId = (await ethers.provider.getNetwork()).chainId;
   //Generate files containining Abis and contract addresses for use in frontend
   generateAbis(chainId, {
