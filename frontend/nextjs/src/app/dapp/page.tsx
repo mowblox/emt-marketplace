0ab15d2e3991da192087e3a5b966bc3f1848141b
Odafe Aror <odafe41@gmail.com>
2023-11-14 07:43:42 +0000

    wip

--- frontend/nextjs/src/app/dapp/page.tsx
+++ frontend/nextjs/src/app/dapp/page.tsx
@@ -2,6 +2,9 @@
 import { Button } from "@/components/ui/button";
 import { useContracts } from "@/lib/hooks/contracts";
 import React from "react";
+import { Tabs, TabsContent, TabsList, TabsTrigger } from "@/components/ui/tabs"
+import PostCard from "@/components/ui/post-card";
+
 
 export default function RootLayout() {
   const { EMTMarketPlace, ExpertToken, MentorToken } = useContracts();
@@ -20,11 +23,28 @@ export default function RootLayout() {
     alert(val);
   }
 
+  // return (
+  //   <div className="flex flex-col">
+  //     <Button onClick={handleEMTMarketPlace}>test EMTMarketPlace</Button>
+  //     <Button onClick={handleExpertToken}>test ExpertToken</Button>
+  //     <Button onClick={handleMentorToken}>test MentorToken</Button>
+  //   </div>
+  // );
+
   return (
     <div className="flex flex-col">
-      <Button onClick={handleEMTMarketPlace}>test EMTMarketPlace</Button>
-      <Button onClick={handleExpertToken}>test ExpertToken</Button>
-      <Button onClick={handleMentorToken}>test MentorToken</Button>
+      <Tabs defaultValue="following" className="w-[400px]">
+        <TabsList>
+          <TabsTrigger value="following">Following</TabsTrigger>
+          <TabsTrigger value="design">Design</TabsTrigger>
+        </TabsList>
+        <TabsContent value="following">
+          <PostCard />
+        </TabsContent>
+        <TabsContent value="design">
+        <PostCard />
+        </TabsContent>
+      </Tabs>
     </div>
   );
 }
