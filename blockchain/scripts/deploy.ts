31aa3edf7644132b0b4b7311bc63a465e7e3d6a0
mickeymond <33749130+mickeymond@users.noreply.github.com>
2023-11-29 05:04:20 +0000

    wip: deploying to topos

--- blockchain/scripts/deploy.ts
+++ blockchain/scripts/deploy.ts
@@ -33,11 +33,19 @@ async function main() {
   console.log("Expert Token deployed at: ", expertToken.target);
 
   // Set MENT & EXPT token addresses
-  await emtMarketplace.setTokenAddresses(mentorToken.target, expertToken.target);
+  console.log("Setting MENT & EXPT Token Addresses!");
+  await (await emtMarketplace.setTokenAddresses(mentorToken.target, expertToken.target)).wait();
+  console.log("Done Setting MENT & EXPT Token Addresses!")
   // Initialize all EXPT levels
-  await emtMarketplace.setExptLevel(1, 1000, 50);
-  await emtMarketplace.setExptLevel(2, 3000, 100);
-  await emtMarketplace.setExptLevel(3, 5000, 200);
+  console.log("Setting EXPT Level 1!");
+  await (await emtMarketplace.setExptLevel(1, 1000, 50)).wait();
+  console.log("Done Setting EXPT Level 1!");
+  console.log("Setting EXPT Level 2!");
+  await (await emtMarketplace.setExptLevel(2, 3000, 100)).wait();
+  console.log("Done Setting EXPT Level 2!");
+  console.log("Setting EXPT Level 3!");
+  await (await emtMarketplace.setExptLevel(3, 5000, 200)).wait();
+  console.log("Done Setting EXPT Level 3!");
 
   const chainId = (await ethers.provider.getNetwork()).chainId;
   //Generate files containining Abis and contract addresses for use in frontend
