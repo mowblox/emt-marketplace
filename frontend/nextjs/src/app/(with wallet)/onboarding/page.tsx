32140dd0d0dd91f11a1748744f3db85ecd665026
Jovells <35954298+Jovells@users.noreply.github.com>
2023-12-04 13:18:04 +0000

    Fixing deployment errors (#36)
    * Jovells workflow auto sync after pr merge (#4)
    
    * Update main.yml
    
    * Update main.yml
    
    * fixed errors, added username & email validation
    
    * added package-lock  to try fix deployment errors

--- frontend/nextjs/src/app/(with wallet)/onboarding/page.tsx
+++ frontend/nextjs/src/app/(with wallet)/onboarding/page.tsx
@@ -1,18 +1,11 @@
 import React from 'react'
 import Image from 'next/image'
 const onboardingPhoto = require('@/assets/onboarding-1.png')
-import { Button } from '@/components/ui/button'
-import Link from 'next/link'
-import { getServerSession } from "next-auth/next"
-import { authOptions } from "@/app/api/auth/[...nextauth]/auth-options"
-import { redirect } from 'next/navigation';
-import { ONBOARDING_PAGE } from '../_components/page-links';
+
+import ButtonToShow from './_components/button-to-show'
+
 
 const OnboardingPage = async () => {
-  const session : any = await getServerSession(authOptions);
-  if (session?.address && !session?.isNotSignedUp) {
-    redirect(ONBOARDING_PAGE(4))
-  }
   return (
     <div className="grid grid-cols-1 md:grid-cols-9 md:h-full gap-4 mx-4 md:mx-8"
     >
@@ -24,10 +17,7 @@ const OnboardingPage = async () => {
         <h4 className='text-xl md:text-3xl text-center font-semibold tracking-wider'>In sed in velit lacus at. Ultricies morbi morbi pharetra nulla eget eget.</h4>
         <p className='text-muted text-center'>Ut accumsan accumsan molestie aliquam feugiat urna quisque eu. Sagittis adipiscing pellentesque massa vulputate curabitur scelerisque.</p>
         <div className="w-full lg:w-2/3 space-y-5 mb-20 md:mb-0">
-          <Button asChild className='w-full'>
-            <Link href={ONBOARDING_PAGE(2)}>Get Started</Link>
-          </Button>
-          <Button className='w-full' variant="outline">I already have an account</Button>
+          <ButtonToShow />
         </div>
       </div>
     </div>
