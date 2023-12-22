1158b63db702a39f4d0fe5be5c579625df1514bd
Odafe Aror <odafe41@gmail.com>
2023-12-22 22:23:46 +0000

    ui: no-data styling

--- frontend/nextjs/src/components/ui/no-data.tsx
+++ frontend/nextjs/src/components/ui/no-data.tsx
@@ -7,8 +7,8 @@ type Props = {
 
 const NoData = ({message="No data loaded"}: Props) => {
   return (
-    <div className='w-full h-full flex justify-center items-center'>
-      <div className="h4">{message}</div>
+    <div className='w-auto h-full flex justify-center items-center  '>
+      <div className="h4 text-sm text-muted bg-accent-shade rounded-md p-2 px-6 mt-4">{message}</div>
     </div>
   )
 }
